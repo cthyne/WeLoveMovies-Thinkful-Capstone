31271779edc1723d130bb899459731cf9d6375c2
Ciara Thyne <ciara.thyne@gmail.com>
2022-02-10 21:21:16 -0800

    fix code issue

--- src/db/migrations/20210409104904_createMoviesTheatersTable.js
+++ src/db/migrations/20210409104904_createMoviesTheatersTable.js
@@ -1,13 +1,24 @@
-exports.up = function (knex) {
-    return knex.schema.createTable("movies_theaters", (table) => {
-      table.integer("movie_id");
-      table.foreign("movie_id").references("movie_id").inTable("movies").onDelete("cascade");
-      table.integer("theater_id");
-      table.foreign("theater_id").references("theater_id").inTable("theaters").onDelete("cascade");
-      table.boolean("is_showing");
-    });
-  };
+exports.up = function(knex) {
+  return knex.schema.createTable("movies_theaters", (table) => {
+      table.integer("movie_id").unsigned().notNullable()
+      table
+        .foreign("movie_id")
+        .references("movie_id")
+        .inTable("movies")
+        .onDelete("CASCADE");
+        
+      table.integer("theater_id").unsigned().notNullable()
+      table
+        .foreign("theater_id")
+        .references("theater_id")
+        .inTable("theaters")
+        .onDelete("CASCADE");
   
-  exports.down = function (knex) {
-    return knex.schema.dropTable("movies_theaters");
-  };
+      table.boolean("is_showing");
+      table.timestamps(true, true);
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("movies_theaters");
+};
